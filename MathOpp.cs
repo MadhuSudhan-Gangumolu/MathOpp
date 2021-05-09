0cf56b18031a04b5aed7ca75031b1516f24b0b86
Madhu Gangumolu <Madhu Gangumolu@COGNINE-L87>
2021-05-09 19:39:09 +0530

    initial commit

---
+++ MathOpp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mathOpp
+{
+    public class MathOpp
+    {
+        public int getIntAddition(int a, int b)
+        {
+            return a + b;
+        }
+        public string getStringAddition(string a,string b)
+        {
+            return (a + b);
+        }
+    }
+}
